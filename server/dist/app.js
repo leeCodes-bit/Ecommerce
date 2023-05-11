afc1dbc4c85e4fe0242b3be7a1c7d5dd4d672687
lilian ovrawah <lilianovrawah@gmail.com>
2023-05-11 12:44:15 +0100

    implement client side

--- server/dist/app.js
+++ server/dist/app.js
@@ -7,6 +7,7 @@ const express_1 = __importDefault(require("express"));
 const morgan_1 = __importDefault(require("morgan"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const cors_1 = __importDefault(require("cors"));
 const buyersRoute_1 = __importDefault(require("./routes/buyersRoute"));
 const db_1 = require("./db/db");
 dotenv_1.default.config();
@@ -14,9 +15,9 @@ const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, morgan_1.default)('dev'));
 app.use((0, cookie_parser_1.default)());
-// app.use(cors({
-//     origin: '*',
-// }));
+app.use((0, cors_1.default)({
+    origin: '*',
+}));
 /*=========Routes======== */
 app.use('/users', buyersRoute_1.default);
 const port = 4000;
--- server/src/app.ts
+++ server/src/app.ts
@@ -12,9 +12,9 @@ const app = express()
 app.use(express.json());
 app.use(logger('dev'));
 app.use(cookieParser())
-// app.use(cors({
-//     origin: '*',
-// }));
+app.use(cors({
+    origin: '*',
+}));
 
 /*=========Routes======== */
 app.use('/users', buyerRouter)
